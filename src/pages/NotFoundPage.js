e8182ee91b091ef8f937f69c58409030c42cb74c
tseng-yc <janice8819@gmail.com>
2020-10-19 11:28:56 +0800

    連到某個頁面或回上一頁

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,11 +1,32 @@
+// 2020-10-19 404頁面不存在+連到某個頁面或回上一頁
+
 import React, { useState, useEffect } from 'react';
+import { useHistory, withRouter } from 'react-router-dom';
 
 function NotFoundPage(props) {
+  // 或使用useHistory勾子也可以達到同樣效果
+  // let history = useHistory()
+
   return (
     <>
       <h1>頁面不存在</h1>
+      <button
+        onClick={() => {
+          props.history.push('/product');
+        }}
+      >
+        到產品頁面
+      </button>
+      <hr />
+      <button
+        onClick={() => {
+          props.history.goBack();
+        }}
+      >
+        到上一頁
+      </button>
     </>
   );
 }
 
-export default NotFoundPage;
+export default withRouter(NotFoundPage);
